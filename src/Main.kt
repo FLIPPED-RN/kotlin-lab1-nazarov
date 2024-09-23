ed9937731eeb620536a63b13d4ef259b5ea6558b
FLIPPED24 <rnazarov142@gmail.com>
2024-09-24 00:38:08 +0200

    complete

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+import kotlin.math.abs
 import kotlin.random.Random
 
 // Назаров Руслан 23-ИСП-3/1
@@ -5,17 +6,28 @@ import kotlin.random.Random
 // одномерные массивы
 
 fun main() {
-    var mas = Array(Random.nextInt(1, 60)) {Random.nextInt(1, 60) -30}
+    var mas = Array(Random.nextInt(1, 60)) {Random.nextInt(1, 60) -15}
     println("сгенерированный массив: " + mas.joinToString(" "))
     println()
 
     var sumOfPositive = 0
+    var modulNumbers = 0
     for (elem in mas) {
         if (elem > 0) {
             sumOfPositive += elem
-            print("$elem ")
+        }
+        else if (elem < 0) {
+            modulNumbers += elem
         }
     }
+
+    val modul = abs(modulNumbers)
+
     println()
-    println("частное от положительных чисел массива: " + sumOfPositive / 2)
+    println("сумма положительных элементов массива: " + sumOfPositive)
+    println("модуль суммы отрицательных элементов: " + modul)
+
+    val c = sumOfPositive / modul
+
+    println("частное от деления суммы положительных элементов массива на модуль суммы отрицательных : $c")
 }
